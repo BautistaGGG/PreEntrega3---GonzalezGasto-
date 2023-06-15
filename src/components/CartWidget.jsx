f897c67f4fa852cfb120605d320db0395c00e9de
unknown <gonzalezbautistagasto@gmail.com>
2023-06-15 11:33:24 -0300

    Hardcodeado el N° de items en el carrito. Listo para entregar

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,24 +1,14 @@
+//Icono importado de https://fontawesome.com/icons/cart-shopping?f=classic&s=solid&pc=%23f5f5f5
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react"
+
+
 function CartWidget() {
-
-  const [numero,setNumero] = useState(0)
-
-  function sumarAlCarrito() {
-    setNumero(prevState => prevState +1)
-  }
   return (
     <>
-      <FontAwesomeIcon 
-        icon={faCartShopping} 
-        style={{color: "white"}} 
-        onClick={sumarAlCarrito}
-      />
-  
-      <p>
-        {numero}
-      </p>
+      <FontAwesomeIcon icon={faCartShopping} style={{color: "white"}} />
+      <p>0</p>
     </>
   )
 }
