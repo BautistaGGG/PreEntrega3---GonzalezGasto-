e09947aa5a8fa85becd3b01f6eccd38b64a5a30f
unknown <gonzalezbautistagasto@gmail.com>
2023-07-04 14:49:16 -0300

    Añadido el input con su funcionalidad de filtrado. Resta organizar mejor el código para mejor legibilidad.

--- src/components/items/ItemListContainer.jsx
+++ src/components/items/ItemListContainer.jsx
@@ -2,32 +2,48 @@
 import ItemCount from "./ItemCount"
 import ItemList from "./ItemList"
 import useFetch from "../hooks/useFetch"
+import { useState } from "react"
 
 function ItemListContainer({greeting}) {
 
   const {APIdata, isLoading} = useFetch("https://6499b22c79fbe9bcf83fccc5.mockapi.io/productos")
-  
-  if (isLoading) {
-    console.log(isLoading)
-    console.log("Cargando...")
-    return(
-      <p className="text-3xl text-center font-extrabold">Cargando data...</p>
-    )
-  }else if(!isLoading){
-    console.log("Data cargada!");
+
+  const [productoBuscado,setProductoBuscado] = useState("")
+  const [filteredData,setFilteredData] = useState("")
+
+  function handleChange(e) {    
+    setProductoBuscado(e.target.value) 
+    setFilteredData(
+      APIdata.filter(product => (product.name.includes(productoBuscado)))
+    )  
   }
 
   return (
     <section className="bg-slate-500 text-center py-4 px-8">
         <h1 className="p-4"> {greeting} </h1>
-          <ItemCount/>
-          <ul className="grid grid-cols-4 gap-4 justify-items-center">
-            {!isLoading && APIdata.map(product => (
+        <ItemCount/>
+        <input 
+          type="text" 
+          placeholder="Busca un producto" 
+          className="px-4 py-2 mb-4"
+          name="search"
+          onChange={handleChange} 
+        />
+
+        {isLoading ? 
+          <p className="text-3xl text-center font-extrabold"> Cargando data...</p> : 
+          <ul className="grid grid-cols-4 gap-4 justify-items-center"> 
+            {filteredData == "" ? APIdata.map(product => (
+            <li className="bg-slate-800 text-yellow-50 rounded-xl max-w-[275px] cursor-pointer transition-all duration-500 hover:scale-105 hover:bg-slate-700" key={product.id}>
+              <ItemList {...product}/>
+            </li>
+            )) : filteredData.map(product => (
               <li className="bg-slate-800 text-yellow-50 rounded-xl max-w-[275px] cursor-pointer transition-all duration-500 hover:scale-105 hover:bg-slate-700" key={product.id}>
                 <ItemList {...product}/>
               </li>
-            ))}
-          </ul>        
+              ))}
+          </ul>
+        }      
     </section>
   )
 }
